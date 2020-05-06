2a7f0300dd31953df89db4d852c57fa3ce800d05
yasburg <yasinaydin@sabanciuniv.edu>
2020-05-06 23:12:23 +0300

    spinner in main screen

--- Assignment 3/NewsStarterExample/app/src/main/java/edu/sabanciuniv/newsstarterexample/MainActivity.java
+++ Assignment 3/NewsStarterExample/app/src/main/java/edu/sabanciuniv/newsstarterexample/MainActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.List;
@@ -16,21 +18,33 @@ import edu.sabanciuniv.newsstarterexample.model.NewsItem;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView newsRecView;
+    Spinner spFilters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Boiler plate code:
         newsRecView = findViewById(R.id.newsrec);
-        NewsAdapter adp = new NewsAdapter(NewsItem.getAllNews(), this, new NewsAdapter.NewsItemClickListener() {
+        spFilters = findViewById(R.id.spfilter);
+
+        String[] filters = getResources().getStringArray(R.array.filters);
+
+        ArrayAdapter<String> adp1 =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, filters);
+
+        spFilters.setAdapter(adp1);
+
+        NewsAdapter adp2 = new NewsAdapter(NewsItem.getAllNews(), this, new NewsAdapter.NewsItemClickListener() {
             @Override
             public void newItemClicked(NewsItem selectedNewsItem) {
                 Toast.makeText(MainActivity.this, selectedNewsItem.getTitle(), Toast.LENGTH_SHORT).show();
             }
         });
         newsRecView.setLayoutManager(new LinearLayoutManager(this));
-        newsRecView.setAdapter(adp);
+        newsRecView.setAdapter(adp2);
+
 
         List<NewsItem> items= NewsItem.getAllNews();
         Log.i("DEV","Total of " + items.size() + " news exist");
